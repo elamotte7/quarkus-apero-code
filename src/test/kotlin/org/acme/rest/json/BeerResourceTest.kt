becda55f727ab5627127b6ffa7a951982a6d49fa
Edouard lamotte <elamotte@sedona.fr>
2020-04-17 18:39:38 +0200

    ordering ut

--- src/test/kotlin/org/acme/rest/json/BeerResourceTest.kt
+++ src/test/kotlin/org/acme/rest/json/BeerResourceTest.kt
@@ -4,21 +4,27 @@ import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
 import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 
 @QuarkusTest
+@TestMethodOrder(OrderAnnotation::class)
 class BeerResourceTest {
 
     @Test
+    @Order(1)
     fun testGetFruits() {
         given()
                 .`when`().get("/beers")
                 .then()
                 .statusCode(200)
-                .body(`is`("[{\"id\":1,\"name\":\"Chouffe\",\"color\":\"Blond\"},{\"id\":2,\"name\":\"Chimay\",\"color\":\"Amber\"},{\"id\":3,\"name\":\"Kwack\",\"color\":\"Amber\"},{\"id\":4,\"name\":\"Ch'ti Triple\",\"color\":\"red\"},{\"id\":5,\"name\":\"Goudale\",\"color\":\"Amber\"}]"))
+                .body(`is`("[{\"id\":1,\"name\":\"Chouffe\",\"color\":\"Yellow\",\"tasted\":false,\"order\":0},{\"id\":2,\"name\":\"Chimay\",\"color\":\"Amber\",\"tasted\":false,\"order\":0},{\"id\":3,\"name\":\"Kwack\",\"color\":\"Amber\",\"tasted\":false,\"order\":0},{\"id\":4,\"name\":\"Ch'ti Triple\",\"color\":\"Yellow\",\"tasted\":false,\"order\":0},{\"id\":5,\"name\":\"Goudale\",\"color\":\"Amber\",\"tasted\":false,\"order\":0}]"))
     }
 
     @Test
+    @Order(2)
     fun testAddFruit() {
         var payload = "{\"name\":\"goudale\",\"color\":\"Amber\",\"tasted\":false,\"order\":0}"
         given()
